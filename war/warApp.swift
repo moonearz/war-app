12b84eea690d8f2cdfcc82b7175d329ffce5cd4f
mbjemaa <munirbenjemaa@gmail.com>
2024-03-22 20:10:28 -0400

    Initial Commit

---
+++ war/warApp.swift
@@ -0,0 +1,17 @@
+//
+//  warApp.swift
+//  war
+//
+//  Created by Munir Ben Jemaa on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct warApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
